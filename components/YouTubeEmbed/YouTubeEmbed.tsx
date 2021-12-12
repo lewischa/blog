a70d324a73e94bece2e4629bfa98da88399fa4d7
Chad Lewis <clewis41092@yahoo.com>
2021-12-11 18:33:36 -0800

    Updated youtube video to be hidden on small screens

--- components/YouTubeEmbed/YouTubeEmbed.tsx
+++ components/YouTubeEmbed/YouTubeEmbed.tsx
@@ -3,9 +3,22 @@ import { YouTube } from 'mdx-embed';
 export function YouTubeEmbed({ id }: { id: string }) {
     return (
         <div className="flex justify-center">
-            <div className="w-4/5">
-                <YouTube youTubeId={id} />
-                <p><em>Prefer to watch instead? Check out the video version!</em></p>
+            <div className="w-4/5 flex flex-col">
+                <div className="hidden md:block">
+                    <YouTube youTubeId={id} aspectRatio="16:9" />
+                </div>
+                <p className="text-center md:text-left">
+                    <em>
+                        Prefer to watch instead? Check out the&nbsp;
+                        <a
+                            href={`https://youtu.be/${id}`}
+                            className="text-blue-600 hover:text-blue-400"
+                        >
+                            YouTube video
+                        </a>
+                        !
+                    </em>
+                </p>
             </div>
         </div>
     );
