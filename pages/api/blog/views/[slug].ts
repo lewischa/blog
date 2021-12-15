e1aef05aa6f8713d445ffd762afb82303c7c9f7e
Chad Lewis <clewis41092@yahoo.com>
2021-12-15 12:11:56 -0800

    Added prisma client and views api

---
+++ pages/api/blog/views/[slug].ts
@@ -0,0 +1,43 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import prisma from 'lib/prisma';
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+    try {
+        console.log('req.query:', req.query);
+        const slug = req.query.slug.toString();
+
+        if (req.method === 'POST') {
+            const updatedViews = await prisma.blogViews.upsert({
+                where: { slug },
+                create: {
+                    slug,
+                    views: 1
+                },
+                update: {
+                    views: {
+                        increment: 1
+                    }
+                }
+            });
+
+            return res.status(200).json({
+                total: updatedViews.views
+            });
+        }
+
+        if (req.method === 'GET') {
+            const views = await prisma.blogViews.findUnique({
+                where: {
+                    slug
+                }
+            });
+
+            return res.status(200).json({ total: views?.views ?? 0 });
+        }
+    } catch (e) {
+        return res.status(500).json({ message: e.message });
+    }
+}
